503e1248ef06ae484486a008f8379df956ef1650
Ritronn <rutvikkale2006@gmail.com>
2025-11-06 19:27:29 +0530

    added login for patients

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import './App.css'
+import Patientlogin from './Pages/Patientlogin'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/patientlogin" element={<Patientlogin/>}/>
+
         {/* Example: <Route path="/Patientdashboard" element={<PatientDashboard />} /> */}
       </Routes>
     </Router>
---
+++ frontend/src/Pages/Patientlogin.jsx
@@ -0,0 +1,8 @@
+function Patientlogin()
+{
+    return(
+
+        <h1>this is patient login</h1>
+    )
+}
+export default Patientlogin
